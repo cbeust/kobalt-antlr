8dfd3cee07648250843e46988b18919468131703
Cedric Beust <cedric@beust.com>
2016-03-17 21:55:30 +0400

    Document AntlrConfig.

--- src/main/kotlin/com/beust/kobalt/plugin/antlr/AntlrConfig.kt
+++ src/main/kotlin/com/beust/kobalt/plugin/antlr/AntlrConfig.kt
@@ -3,12 +3,23 @@ package com.beust.kobalt.plugin.antlr
 import com.beust.kobalt.api.Project
 
 /**
- * The content of the antlr{} directive.
+ * The content of the antlr{} directive, which defines how ANTLR runs.
  */
 class AntlrConfig(val project: Project) {
-    // e.g. src/main/antlr
-    var setName = "antlr"
-    var generatedSourceDir = "src/generated/antlr"
+    /**
+     * The location of ANTLR grammars.
+     */
     var antlrSourceDir = "src/main/antlr"
+
+    /**
+     * Where ANTLR generates its source files from the grammars you provide.
+     */
+    var generatedSourceDir = "src/generated/antlr"
+
+    /**
+     * The name of the source set, used in the last segment of `src/main` or `src/test`, e.g. `src/main/antlr`.
+     */
+    var setName = "antlr"
+
 }
 
